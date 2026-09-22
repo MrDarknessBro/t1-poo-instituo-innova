f066781f77fa577bbb37d19f0b31e3d7a90d4dd6
UCA40417 <UCA40417@UCA40417>
2026-09-21 20:49:23 -0500

    feat(t1): crear estructura base con Scanner y ArrayList

---
+++ src/t1/T1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author UCA40417
+ */
+public class T1 {
+ 
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+        ArrayList<Alumno> listaAlumnos = new ArrayList<>();
+        
+        System.out.println("=== Sistema Instituto Innova ===");
+        
+        scanner.close();
+    }
+}
